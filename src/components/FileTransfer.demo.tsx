38f2384850aa54062c0d595628cd608be797e462
sam <sschimmelpfennig@proton.me>
2025-06-18 11:00:18 +0200

    docs: create playwright.md

---
+++ src/components/FileTransfer.demo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FileTransfer from './FileTransfer';
+
+export default function FileTransferDemo() {
+  return (
+    <div style={{ width: 600 }}>
+      <FileTransfer />
+    </div>
+  );
+}
