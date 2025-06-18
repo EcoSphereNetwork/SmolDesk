4d1f49212e60e475bc459653ec05297328d56d2a
sam <sschimmelpfennig@proton.me>
2025-06-18 19:06:43 +0200

    test(storybook): add automated screenshot snapshots

---
+++ tests/unit/components/ConnectionManager.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { composeStory } from '@storybook/testing-react';
+import * as stories from '../../../src/components/ConnectionManager.stories';
+
+declare module '@storybook/react' { interface StoryObj<TArgs = any> { args?: Partial<TArgs>; } }
+
+const Default = composeStory(stories.Default, stories.default);
+
+test.skip('renders ConnectionManager', () => {
+  const { container } = render(<Default />);
+  expect(container.innerHTML).toMatchSnapshot();
+});
