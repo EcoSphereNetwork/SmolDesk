bb67ed28b4900f281a2a93aa5355be6e699c3d1f
sam <sschimmelpfennig@proton.me>
2025-07-07 08:41:28 +0200

    feat(mobile): initialize React Native project

---
+++ mobile/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => (
+  <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>SmolDesk Mobile</Text>
+  </SafeAreaView>
+);
+
+export default App;
